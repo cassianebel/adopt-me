1702dc3668fb8d9a8f78fb2535e1fe7ece414c34
Cassia Nebel <162936759+cassianebel@users.noreply.github.com>
2024-07-27 12:17:25 -0500

    results component

---
+++ src/Results.jsx
@@ -0,0 +1,24 @@
+import Pet from "./Pet";
+
+const Results = ({ pets }) => {
+  return (
+    <div className="search">
+      {!pets.length ? (
+        <h1>No Pets Found</h1>
+      ) : (
+        pets.map((pet) => (
+          <Pet
+            key={pet.id}
+            animal={pet.animal}
+            breed={pet.breed}
+            name={pet.name}
+            images={pet.images}
+            location={`${pet.city}, ${pet.state}`}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Results;
